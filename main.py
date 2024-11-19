318ca73d34472cf3ccee98c8bdca07c7031abdf7
DESKTOP-0LO6JHC\Jacob <simplafyer@gmail.com>
2024-11-19 14:18:30 -0500

    commented out troublesome code to get random forest working

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def go(config: DictConfig):
                         "val_size": config['modeling']['val_size'],
                         "random_seed": config['modeling']['random_seed'],
                         "stratify_by": config['modeling']['stratify_by'],
-                        "rf_config": fp,
+                        "rf_config": rf_config,
                         "max_tfidf_features": config['modeling']['max_tfidf_features'],
                         "output_artifact": "random_forest_export",
                 },
